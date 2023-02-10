3b73185ff80a71f3dccbcfd3e2bb0f0a84851eef
joao-muzange <joaonovais101@icloud.com>
2023-02-10 16:46:33 +0000

    bugs

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,12 @@
 <?php
 
+use App\Http\Controllers\EspecialidadeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::get('especialidades', EspecialidadeController::class);
+ Route::get('/especialidades', [EspecialidadeController::class,'index']);
+// Route::post('/especialidades/{request}', [EspecialidadeController::class,'create']);
+// Route::put('/especialidades', [EspecialidadeController::class,'update']);
+// Route::delete('/especialidades', [EspecialidadeController::class,'delete']);
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EspecialidadeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/especialidades', [EspecialidadeController::class,'getAll']);
+// Route::get('/especialidades/{id}', [EspecialidadeController::class,'get']);
+// Route::post('/especialidades/{request}', [EspecialidadeController::class,'create']);
+// Route::put('/especialidades', [EspecialidadeController::class,'update']);
+// Route::delete('/especialidades', [EspecialidadeController::class,'delete']);
